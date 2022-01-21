0a5e2cbd97f18e050708882cac443ff5b8577e2f
Nicholas Tapia <tapia.nicholas@gmail.com>
2022-01-20 19:30:43 -0500

    complete beta

--- script.js
+++ script.js
@@ -7,30 +7,58 @@
 // sudo code
 
 // let photos = location of all photos
-// let previous = photos[0]
-// let current = photos[1]
-// for (let i=2; i<photos.length; i++}
+// for (let i=i; i<photos.length; i++}\
+//   let previous = photos[i-1]
+//   let current = photos[i]
+//   set top in dom
 //   if current.area intersects previous.area
-//     current.x += previous.x
-//   current = photos[i]
-//   previous = photos[i-1]
+//     current.left += previous.left
+// set top in dom of last photo
 
 console.log('it works')
 let photos = document.querySelectorAll('img')
-console.log(photos)
-let previous = photos[0]
-let current = photos[1]
-console.log(previous.attributes["data-top"].value)
-previous.style.top = previous.attributes["data-top"].value
-// console.log('x')
-// console.log(current.x)
-// console.log('width')
-// console.log(current.width)
-// console.log('offset width')
-// console.log(current.offsetWidth)
-// console.log('bounding client rectangle')
-// console.log(current.getBoundingClientRect() )
-for (let i=2; i<photos.length; i++){
-    console.log(i)
+for (let i=1; i<photos.length; i++){
+    let previous = photos[i-1]
+    let current = photos[i]
+    set_top_in_dom(previous)
+    let [previousTop, previousBottom, previousLeft, previousRight] = get_coordinates(previous)
+    let [currentTop, currentBottom, currentLeft, currentRight] = get_coordinates(current)
+    if (check_intersect(
+        previousTop, previousBottom, previousLeft, previousRight,
+        currentTop, currentBottom, currentLeft, currentRight)) {
+            current.style.left = 10 + current.x + previous.width + "px" // margin!
+        }
 }
-console.log(photos[0].x)
+set_top_in_dom(photos[photos.length - 1])
+
+
+/* functions */
+
+
+function set_top_in_dom (box) {
+    box.style.top = box.attributes["data-top"].value
+}
+
+function check_intersect( // true = intersects, false = does
+    RectATop, RectABottom, RectALeft, RectARight,
+    RectBTop, RectBBottom, RectBLeft, RectBRight) {
+        if (RectALeft < RectBRight && RectARight > RectBLeft &&
+        RectATop < RectBBottom && RectABottom > RectBTop ) {
+            console.log("returns true, current intersects with previous")
+            return true
+        } else {
+            console.log("returns false, no intersection")
+            return false
+        }
+ 
+}
+
+function get_coordinates(box) {
+    let scale = box.naturalWidth / box.width
+    let top = parseInt(box.attributes["data-top"].value.slice(0,-2))
+    let bottom = top + box.naturalHeight/scale // is there a better way to find image height?
+    let left = box.x
+    let right = box.x + box.width
+    console.log([top, bottom, left, right])
+    return [top, bottom, left, right]
+}
